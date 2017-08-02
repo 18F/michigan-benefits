2b012d3232cae694b33f5aa5a9e9bde68744b3fa
Adarsh Pandit <adarshpandit@gmail.com>
2017-08-02 15:57:49 -0700

    Use the address of MIHHS office in Flint

--- spec/factories/snap_applications.rb
+++ spec/factories/snap_applications.rb
@@ -3,11 +3,11 @@ FactoryGirl.define do
     email "test@example.com"
     name "RJD2"
     birthday 50.years.ago
-    street_address "123 Main St."
+    street_address "125 E Union St"
     city "Flint"
     county "Genesee"
     state "MI"
-    zip "48501"
+    zip "48502"
     signature "Mr. RJD2"
     signed_at Date.current
   end
